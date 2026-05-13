56f3c8b3dead96dac019366f8db0d09f5b07f353
armanizadi99 <armanizadi9999@gmail.com>
2026-05-13 07:34:30 +0330

    Added a controller for TodoListTemplate.

---
+++ UniTodo/Modules/Todos/Api/Controllers/TodoListTemplateController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using UniTodo.Modules.Todos.Application.DTOs;
+using UniTodo.Modules.Todos.Application.Services;
+
+namespace UniTodo.Modules.Todos.Api.Controllers
+{
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+    public class TodoListTemplateController : ControllerBase
+    {
+        private readonly ITodoListTemplateService _TodoListTemplateService;
+
+public TodoListTemplateController(ITodoListTemplateService todoListTemplateService)
+{
+        _TodoListTemplateService = todoListTemplateService;
+        }
+
+[HttpGet]
+public async Task<ActionResult<List<TodoListTemplateDto>>> GetAllTodoListTemplatesForCurrentUserAsync()
+{
+        var result = await _TodoListTemplateService.GetUserTodoListsAsync();
+
+        return Ok(result);
+        }
+
+[HttpPost]
+public async Task<ActionResult<int>> CreateTodoListTemplateAsync([FromBody] CreateTodoListTemplateDto dto)
+{
+        var result = await _TodoListTemplateService.CreateTodoListTemplateAsync(dto);
+
+        return CreatedAtAction(nameof(GetTodoListTemplateByIdAsync), new { id = result.Id }, result);
+        }
+
+[HttpGet("{id:int:min(1)}")]
+public async Task<ActionResult<TodoListTemplateDto>> GetTodoListTemplateByIdAsync([FromRoute] int id)
+{
+        var result = await _TodoListTemplateService.GetTodoListTemplateByIdAsync(id);
+
+        return Ok(result);
+        }
+    }
+}
