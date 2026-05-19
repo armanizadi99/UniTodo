9d4abe193a7065bef8bc9c33f416d3ffa561e962
armanizadi99 <armanizadi9999@gmail.com>
2026-05-19 09:56:04 +0330

    Modified how a user is authenticated.

--- UniTodo/Program.cs
+++ UniTodo/Program.cs
@@ -1,20 +1,37 @@
 using Microsoft.AspNetCore.Authentication.BearerToken;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using OpenIddict.Validation.AspNetCore;
+using System.Text;
+using UniTodo.Modules.Auth;
 using UniTodo.Modules.Todos.Infrastructure;
 using UniTodo.Modules.Todos.ModuleStartup;
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
-
 builder.Services.AddAuthentication(options =>
 {
-options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
 })
-.AddJwtBearer(); ;
-
-
+.AddJwtBearer(options =>
+{
+options.Audience = "for my todo app";
+options.ClaimsIssuer = "my own app";
+options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+{
+ValidAudience = "for my todo app",
+ValidateAudience = true,
+ValidIssuer = "my own app",
+    ValidateIssuer = true,
+ValidateIssuerSigningKey = false,
+IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("thisisjut a fucking temp passwword I'll change it later, I just need to test something right now. Hardcoding such a code in my codebase might not be a big security issue because this code is running on a server trusted server, but well, because other parts of the applications might require it or well gonna right the rest."))
+};
+});
 
 builder.Services.AddControllers();
 builder.Services.AddTodoModule(builder.Configuration.GetSection("TodoModule"));
+builder.Services.AddAuthModule(builder.Configuration.GetSection("AuthModule"));
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 var app = builder.Build();
