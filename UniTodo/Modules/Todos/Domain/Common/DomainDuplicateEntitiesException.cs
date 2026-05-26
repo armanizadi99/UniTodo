2e283d237008696e9e6a18aff4f4f4ad06371248
armanizadi99 <armanizadi9999@gmail.com>
2026-05-26 11:01:37 +0330

    feat(todo): add AddTodoItemTemplate and Delete methods to TodoListTemplate aggregate

---
+++ UniTodo/Modules/Todos/Domain/Common/DomainDuplicateEntitiesException.cs
@@ -0,0 +1,7 @@
+﻿namespace UniTodo.Modules.Todos.Domain.Common
+{
+    internal class DomainDuplicateEntitiesException : DomainException
+    {
+        internal DomainDuplicateEntitiesException(string message) : base(message) { }
+    }
+}
--- UniTodo/Modules/Todos/Domain/Entities/TodoListTemplate.cs
+++ UniTodo/Modules/Todos/Domain/Entities/TodoListTemplate.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection.Metadata;
+using UniTodo.Modules.Todos.Application.Common;
 using UniTodo.Modules.Todos.Domain.Common;
 using UniTodo.Modules.Todos.Domain.Enums;
 using UniTodo.Modules.Todos.Domain.ValueObjects;
@@ -16,33 +17,52 @@ namespace UniTodo.Modules.Todos.Domain.Entities
 
         internal IReadOnlyCollection<TodoItemTemplate> TodoItemTemplates => _todoItemTemplates.AsReadOnly();
 
-internal TodoListTemplate( UserId ownerId, string name, ResetPolicy resetPolicy)
+        internal TodoListTemplate(UserId ownerId, string name, ResetPolicy resetPolicy)
         {
-        if (!ResetPolicy.IsDefined(resetPolicy))
-            throw new ArgumentOutOfRangeException($"{nameof(resetPolicy)} is not defined.");
-        ArgumentException.ThrowIfNullOrEmpty(name);
-        OwnerId = ownerId;
-        Name = name;
-        ResetPolicy = resetPolicy;
-        Status = TodoListStatus.Active;
+            if (!ResetPolicy.IsDefined(resetPolicy))
+                throw new ArgumentOutOfRangeException($"{nameof(resetPolicy)} is not defined.");
+            ArgumentException.ThrowIfNullOrEmpty(name);
+            OwnerId = ownerId;
+            Name = name;
+            ResetPolicy = resetPolicy;
+            Status = TodoListStatus.Active;
         }
 
-internal void Archive(UserId actorUserId)
-{
-        if (OwnerId != actorUserId)
-            throw new DomainNotAuthorizedException();
-        if (Status == TodoListStatus.Archived)
-            throw new DomainInvalidOperationException("This todo list is already archived.");
-        Status = TodoListStatus.Archived;
+        internal void Archive(UserId actorUserId)
+        {
+            if (OwnerId != actorUserId)
+                throw new DomainNotAuthorizedException();
+            if (Status == TodoListStatus.Archived)
+                throw new DomainInvalidOperationException("This todo list is already archived.");
+            Status = TodoListStatus.Archived;
         }
 
-internal void MakeActive(UserId actorUserId)
-{
-        if (OwnerId != actorUserId)
-            throw new DomainNotAuthorizedException();
-        if (Status == TodoListStatus.Active)
-            throw new DomainInvalidOperationException("This todo list is already active.");
-        Status = TodoListStatus.Active;
+        internal void MakeActive(UserId actorUserId)
+        {
+            if (OwnerId != actorUserId)
+                throw new DomainNotAuthorizedException();
+            if (Status == TodoListStatus.Active)
+                throw new DomainInvalidOperationException("This todo list is already active.");
+            Status = TodoListStatus.Active;
+        }
+
+        internal void AddTodoItemTemplate(TodoItemTemplate todoItemTemplate, UserId actorUserId)
+        {
+            if (OwnerId != actorUserId)
+                throw new DomainNotAuthorizedException();
+            if (TodoItemTemplates.Any(e => e.Description.Value.Equals(todoItemTemplate.Description.Value, StringComparison.OrdinalIgnoreCase)))
+                throw new DomainDuplicateEntitiesException("No duplicate descriptions allowed in a TodoList.");
+            _todoItemTemplates.Add(todoItemTemplate);
+        }
+
+        internal void Delete(int id, UserId actorId)
+        {
+            if (OwnerId != actorId)
+                throw new DomainNotAuthorizedException();
+            var todoItemTemplate = _todoItemTemplates.Where(t => t.Id == id).FirstOrDefault();
+            if (todoItemTemplate is null)
+                throw new DomainEntityNotFoundException("TodoItemTemplate doesn't exist", id);
+            _todoItemTemplates.Remove(todoItemTemplate);
         }
     }
 }
