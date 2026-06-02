413270a7200bbaa06fc5bb00d9901c1c56562e26
armanizadi99 <armanizadi9999@gmail.com>
2026-06-02 12:08:56 +0330

    Added another overload for the GetTodoListRunByIdAsync, so we could retreive a list with only one item included as well.

--- UniTodo/Modules/Todos/Application/Interfaces/ITodoListRunRepository.cs
+++ UniTodo/Modules/Todos/Application/Interfaces/ITodoListRunRepository.cs
@@ -6,6 +6,7 @@ namespace UniTodo.Modules.Todos.Application.Interfaces
     public interface ITodoListRunRepository : IRepository<TodoListRun>
     {
         Task<TodoListRun?> GetTodoListRunByIdAsync(int id, bool includeItems = false, CancellationToken cancellationToken = default);
+        Task<TodoListRun?> GetTodoListRunByIdAsync( int id, int itemId, CancellationToken cancellationToken = default );
         Task<IReadOnlyList<TodoListRun>> GetUserActiveRunsAsync(Guid userId, CancellationToken cancellationToken = default);
     }
 }
