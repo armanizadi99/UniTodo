da2e7922b7df9fa858a7b9b4e0e58093d2c167f4
armanizadi99 <armanizadi9999@gmail.com>
2026-06-03 10:54:40 +0330

    Removed the null or empty constraint from TodoItemNotes since it really could be an empty thing.

--- UniTodo/Modules/Todos/Domain/ValueObjects/TodoItemNotes.cs
+++ UniTodo/Modules/Todos/Domain/ValueObjects/TodoItemNotes.cs
@@ -8,8 +8,6 @@ namespace UniTodo.Modules.Todos.Domain.ValueObjects
 
         public TodoItemNotes(string value)
         {
-            if (string.IsNullOrEmpty(value))
-                throw new DomainArgumentException(nameof(TodoItemNotes), "Notes couldn't be empty or whitespace.");
             if (value.Length > Constants.NotesMaxLength)
                 throw new DomainArgumentException(nameof(TodoItemNotes), $"Notes couldn't be longer than {Constants.NotesMaxLength} characters");
 
