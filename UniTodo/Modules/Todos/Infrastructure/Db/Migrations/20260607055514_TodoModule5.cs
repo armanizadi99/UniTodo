fa26b44f3c45b3b3f2178f9348405f6cf804a04f
armanizadi99 <armanizadi9999@gmail.com>
2026-06-07 09:26:32 +0330

    Added a new migration to add the schema changes to the database.

---
+++ UniTodo/Modules/Todos/Infrastructure/Db/Migrations/20260607055514_TodoModule5.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniTodo.Modules.Todos.Infrastructure.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class TodoModule5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_todoItemTemplates_Description",
+                table: "todoItemTemplates");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CompletedBy",
+                table: "todoItems",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_todoItemTemplates_Description_TodoListId",
+                table: "todoItemTemplates",
+                columns: new[] { "Description", "TodoListId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_todoItemTemplates_Description_TodoListId",
+                table: "todoItemTemplates");
+
+            migrationBuilder.DropColumn(
+                name: "CompletedBy",
+                table: "todoItems");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_todoItemTemplates_Description",
+                table: "todoItemTemplates",
+                column: "Description",
+                unique: true);
+        }
+    }
+}
