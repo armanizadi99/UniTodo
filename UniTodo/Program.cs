9e8f8b0d0fc19b0c4274a8c152ca59d69815ba0a
armanizadi99 <armanizadi9999@gmail.com>
2026-06-08 11:18:48 +0330

    Configured swagger to allow authorization in the ui.

--- UniTodo/Program.cs
+++ UniTodo/Program.cs
@@ -1,7 +1,8 @@
 using Microsoft.AspNetCore.Authentication.BearerToken;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
-using OpenIddict.Validation.AspNetCore;
+using Microsoft.OpenApi;
 using System.Text;
 using System.Text.Json.Serialization;
 using UniTodo.Modules.Auth;
@@ -35,11 +36,34 @@ builder.Services.AddControllers()
     options.JsonSerializerOptions.Converters
     .Add(new JsonStringEnumConverter(System.Text.Json.JsonNamingPolicy.CamelCase));
 });
+
 builder.Services.AddTodoModule(builder.Configuration.GetSection("TodoModule"));
 builder.Services.AddAuthModule(builder.Configuration.GetSection("AuthModule"));
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+options.SwaggerDoc("v1", new OpenApiInfo { Title = "uni todo", Version = "1.0" });
+
+options.AddSecurityDefinition("Jwt bearer", new OpenApiSecurityScheme
+{
+    Name = "Authorization",
+    Type = SecuritySchemeType.Http,
+    Scheme = "Bearer",
+    BearerFormat = "JWT",
+    In = ParameterLocation.Header,
+    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1Ni...\""
+});
+
+options.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+{
+{
+new OpenApiSecuritySchemeReference("Jwt bearer", document),
+new List<string>()
+    }
+});
+
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
