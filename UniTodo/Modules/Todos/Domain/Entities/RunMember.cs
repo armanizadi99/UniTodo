c9df113fdee89c9aa965ff9335d7b4b12f71c04e
armanizadi99 <armanizadi9999@gmail.com>
2026-06-09 10:12:13 +0330

    Added the entity RunMember.

---
+++ UniTodo/Modules/Todos/Domain/Entities/RunMember.cs
@@ -0,0 +1,19 @@
+﻿using UniTodo.Modules.Todos.Domain.Common;
+using UniTodo.Modules.Todos.Domain.ValueObjects;
+
+namespace UniTodo.Modules.Todos.Domain.Entities
+{
+    public class RunMember : EntityBase
+    {
+public UserId UserId { get; private set; }
+public int RunId { get; private set; }
+        public TodoListRun Run { get; private set; } = null!;
+
+        private RunMember() { }
+
+public RunMember(UserId userId)
+{
+        UserId = userId;
+}
+    }
+}
