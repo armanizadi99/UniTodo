6c2ee0c718fda44e8c8516cc7bd91ca022f6a49d
armanizadi99 <armanizadi9999@gmail.com>
2026-06-12 10:45:35 +0330

    chore: add migration for ResetsAt property in TodoListRun

--- UniTodo/Modules/Todos/Infrastructure/Db/Migrations/20260612071402_AddResetsAtToTodoListRun.Designer.cs
+++ UniTodo/Modules/Todos/Infrastructure/Db/Migrations/20260612071402_AddResetsAtToTodoListRun.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UniTodo.Modules.Todos.Infrastructure.Db;
 
@@ -10,9 +11,11 @@ using UniTodo.Modules.Todos.Infrastructure.Db;
 namespace UniTodo.Modules.Todos.Infrastructure.Db.Migrations
 {
     [DbContext(typeof(TodoDbContext))]
-    partial class TodoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260612071402_AddResetsAtToTodoListRun")]
+    partial class AddResetsAtToTodoListRun
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.15");
---
+++ UniTodo/Modules/Todos/Infrastructure/Db/Migrations/20260612071402_AddResetsAtToTodoListRun.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniTodo.Modules.Todos.Infrastructure.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddResetsAtToTodoListRun : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "ResetsAt",
+                table: "todoListRuns",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResetsAt",
+                table: "todoListRuns");
+        }
+    }
+}
