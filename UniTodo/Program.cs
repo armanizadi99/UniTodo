3f2b887a8c7c3bcd4beaad377487aa095b8c95a6
armanizadi99 <armanizadi9999@gmail.com>
2026-06-15 13:18:06 +0330

    Added serilog and configured it to write logs to logs/log-.json with a rolling interval of one day.

--- UniTodo/Program.cs
+++ UniTodo/Program.cs
@@ -3,64 +3,84 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi;
+using Serilog;
+using Serilog.Formatting.Json;
 using System.Text;
 using System.Text.Json.Serialization;
 using UniTodo.Modules.Auth;
 using UniTodo.Modules.Todos.Infrastructure;
 using UniTodo.Modules.Todos.ModuleStartup;
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers()
-.AddJsonOptions(options =>
+Log.Logger = new LoggerConfiguration()
+.MinimumLevel.Warning()
+.WriteTo.File(new JsonFormatter(), "logs/log-.json", rollingInterval: RollingInterval.Day)
+.CreateLogger();
+try
 {
-    options.JsonSerializerOptions.Converters
-    .Add(new JsonStringEnumConverter(System.Text.Json.JsonNamingPolicy.CamelCase));
-});
+    Log.Information("Starting application");
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddTodoModule(builder.Configuration.GetSection("TodoModule"));
-builder.Services.AddAuthModule(builder.Configuration.GetSection("AuthModule"));
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo { Title = "uni todo", Version = "1.0" });
-
-    options.AddSecurityDefinition("Jwt bearer", new OpenApiSecurityScheme
+    builder.Host.UseSerilog();
+    // Add services to the container.
+    builder.Services.AddControllers()
+    .AddJsonOptions(options =>
     {
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1Ni...\""
+        options.JsonSerializerOptions.Converters
+    .Add(new JsonStringEnumConverter(System.Text.Json.JsonNamingPolicy.CamelCase));
     });
 
-    options.AddSecurityRequirement(document => new OpenApiSecurityRequirement
-{
+    builder.Services.AddTodoModule(builder.Configuration.GetSection("TodoModule"));
+    builder.Services.AddAuthModule(builder.Configuration.GetSection("AuthModule"));
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(options =>
+    {
+        options.SwaggerDoc("v1", new OpenApiInfo { Title = "uni todo", Version = "1.0" });
+
+        options.AddSecurityDefinition("Jwt bearer", new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1Ni...\""
+        });
+
+        options.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+    {
 {
 new OpenApiSecuritySchemeReference("Jwt bearer", document),
 new List<string>()
     }
-});
+    });
 
-});
-var app = builder.Build();
+    });
+    var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.UseSerilogRequestLogging();
+
+    app.MapControllers();
+    app.MapTodoEndpoints();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.UseHttpLogging();
-
-app.MapControllers();
-app.MapTodoEndpoints();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
